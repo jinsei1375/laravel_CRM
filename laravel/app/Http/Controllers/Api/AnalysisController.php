d30e171dbd6ce8ad2eaf51d03541bd1822e988f1
murakami <murakami@kokochie.jp>
2023-08-01 22:20:24 +0900

    日別の売上

--- laravel/app/Http/Controllers/Api/AnalysisController.php
+++ laravel/app/Http/Controllers/Api/AnalysisController.php
@@ -5,13 +5,32 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
     public function index(Request $request)
     {
+        $subQuery = Order::BetweenDate($request->startDate, $request->endDate);
+        if($request->type == 'perDay') {
+            $subQuery->where('status', true)
+            ->groupBy('id')
+            ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date');
+
+            $data = DB::table($subQuery)
+            ->groupBy('date')
+            ->selectRaw('date, sum(totalPerPurchase) as total')
+            ->get();
+
+            $labels = $data->pluck('date');
+            $totals = $data->pluck('total');
+        }
         return response()->json([
-            'data' => $request->startDate,
+            'data' => $data,
+            'status' => $request->type,
+            'labels' => $labels,
+            'totals' => $totals,
         ], Response::HTTP_OK);
     }
 }
--- laravel/app/Models/Order.php
+++ laravel/app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Scopes\Subtotal;
+use Illuminate\Support\Carbon;
 
 class Order extends Model
 {
@@ -22,9 +23,11 @@ class Order extends Model
         } elseif (!is_null($startDate) && is_null($endDate)) {
             return $query->where('created_at', ">=", $startDate);
         } elseif (is_null($startDate) && !is_null($endDate)) {
-            return $query->where('created_at', "<=", $endDate);
+            $endDate1 = Carbon::parse($endDate)->addDay(1);
+            return $query->where('created_at', "<=", $endDate1);
         } elseif (!is_null($startDate) && !is_null($endDate)) {
-            return $query->where('created_at', "<=", $endDate)
+            $endDate1 = Carbon::parse($endDate)->addDay(1);
+            return $query->where('created_at', "<=", $endDate1)
             ->where('created_at', ">=", $startDate);
         }
     }
